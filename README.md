c2c46ec16ab18630ab8f08838710a640b39f3acb
1095661190@qq.com <1095661190@qq.com>
2020-06-11 17:23:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# maven project demo 
+use maven plugin 打包项目 
+父pom assembly plugin 生成 jar jar-with-dependencies  zip包(可在src/main/assembly/assembly.xml配置)
+
+
---
+++ src/main/java/com/project/server/App.java
@@ -0,0 +1,12 @@
+package com.project.server;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
